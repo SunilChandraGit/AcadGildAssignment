4238ac7decd926d7f40737850c367933e3cc677c
neeil93 <chandrasunil93@gmail.com>
2018-03-15 20:38:40 +0530

    Initial commit

---
+++ Assignment-1.2/CSPrint.py
@@ -0,0 +1,6 @@
+a = '';
+tu=[];
+for i in range(2000, 3201):
+    if i%7==0 and i%5!=0:
+        a=a+str(i)+',';
+print(a[0:-1]);
---
+++ Assignment-2.1/PrintList.py
@@ -0,0 +1,4 @@
+a=input('Please enter a comma separated list of numbers : ');
+b=a.split(',');
+for i in b:
+    print(i);
